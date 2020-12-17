7dfeb3dc87644a3df166bde2f059a5daaa336989
TheKUBS <kubalido1@o2.pl>
2020-12-17 21:46:48 +0100

    chat and movie settings added

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./css/Player.css";
+import ReactLoading from "react-loading";
 
 const Player = () => {
   return (
     <div className="player">
-      <div className="movie-holder"></div>
+      <div className="movie-holder">
+        <ReactLoading
+          type={"spin"}
+          color={"white"}
+          height={"5vh"}
+          width={"3vw"}
+        />
+      </div>
       <div className="movie-controls">
-        <button>-5s</button>
-        <button>start/stop</button>
-        <button>+5s</button>
+        <button>
+          <i className="fas fa-angle-double-left"></i>
+        </button>
+        <button>
+          <i className="fas fa-play"></i>
+        </button>
+        <button>
+          <i className="fas fa-angle-double-right"></i>
+        </button>
+        <button className="volume-icon">
+          <i className="fas fa-volume-down"></i>
+        </button>
+        <button className="settings-icon">
+          <i className="fas fa-sliders-h"></i>
+        </button>
       </div>
     </div>
   );
